20f9ce5821588ace419cb98a1283fd0db15b9598
Prince Jangra <princejangra044@gmail.com>
2026-03-29 11:25:33 +0530

    Refactor MySQL connection settings in db.js
    Updated database connection configuration to include port and removed default values.

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || 'localhost',
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || 'Prince12093',
-  database: process.env.MYSQLDATABASE || 'calendly_clone',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   dateStrings: true,
